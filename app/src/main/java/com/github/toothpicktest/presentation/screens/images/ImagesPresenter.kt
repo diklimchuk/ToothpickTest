01f0e2c1691b521c39c68468707146ea9c1396fb
daniil <klimchuk.daniil@gmail.com>
2018-05-23 10:42:08 +0300

    Implement pagination in presenter

--- app/src/main/java/com/github/toothpicktest/presentation/screens/images/ImagesPresenter.kt
+++ app/src/main/java/com/github/toothpicktest/presentation/screens/images/ImagesPresenter.kt
@@ -5,6 +5,7 @@ import com.github.toothpicktest.domain.repo.IImageRepo
 import com.github.toothpicktest.presentation.mvp.BasePresenter
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import io.reactivex.subjects.PublishSubject
 import timber.log.Timber
 import java.util.Date
 import javax.inject.Inject
@@ -16,15 +17,51 @@ class ImagesPresenter @Inject constructor(
         private val repo: IImageRepo
 ) : BasePresenter<ImagesView>() {
 
-    override fun attachView(view: ImagesView?) {
-        super.attachView(view)
-        getImages()
+    companion object {
+        private const val IMAGE_BLOCK_SIZE = 100
+
+        private const val LOAD_THRESHOLD = 20
+
+        private val MAX_DATE = Date(Long.MAX_VALUE)
     }
 
-    private fun getImages() {
-        repo.getImages(1, 100)
+    private var lastRequestedPage = 0
+
+    private val pageRequests = PublishSubject.create<PageRequest>()
+
+    override fun onFirstViewAttach() {
+        super.onFirstViewAttach()
+        observePageRequests()
+        pageRequests.onNext(createPageRequest(1, MAX_DATE))
+    }
+
+    fun onScolledToPosition(
+            itemUploadDate: Date,
+            margin: Int
+    ) {
+        if (margin < LOAD_THRESHOLD) {
+            val nextPage = lastRequestedPage
+            pageRequests.onNext(createPageRequest(lastRequestedPage, itemUploadDate))
+            lastRequestedPage = nextPage
+        }
+    }
+
+    private fun observePageRequests() {
+        pageRequests
+                .concatMapSingle { request ->
+                    repo.getImages(request.page, IMAGE_BLOCK_SIZE)
+                            .map { it.filter { it.uploadDate < request.getMaxUploadDate() } }
+                }
+                .filter { it.isNotEmpty() }
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe({ viewState.showImages(it) }, { Timber.e(it) })
     }
+
+    private fun PageRequest.getMaxUploadDate() = Date(orderValueLt)
+
+    private fun createPageRequest(
+            page: Int,
+            maxUploadDate: Date
+    ) = PageRequest(page, maxUploadDate.time)
 }
--- app/src/main/java/com/github/toothpicktest/presentation/screens/images/ImagesView.kt
+++ app/src/main/java/com/github/toothpicktest/presentation/screens/images/ImagesView.kt
@@ -1,12 +1,12 @@
 package com.github.toothpicktest.presentation.screens.images
 
-import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy
+import com.arellomobile.mvp.viewstate.strategy.AddToEndStrategy
 import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
 import com.github.toothpicktest.domain.entity.Image
 import com.github.toothpicktest.presentation.mvp.BaseView
 
 interface ImagesView : BaseView {
 
-    @StateStrategyType(AddToEndSingleStrategy::class)
+    @StateStrategyType(AddToEndStrategy::class)
     fun showImages(images: Collection<Image>)
 }
